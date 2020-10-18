7e0b5135c428d4a707a829ace84734e4db904d57
Aluferox <aluferoxx@hotmail.com>
2020-10-18 19:35:22 -0300

    inplementando CreateView

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('empresa/', include('apps.empresas.urls')),
+
 ]
