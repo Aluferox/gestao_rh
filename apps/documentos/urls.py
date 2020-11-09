f8616e7d4ca2313abb440997c1d2e7e2c5555a60
Aluferox <aluferoxx@hotmail.com>
2020-11-09 02:08:54 -0300

    CRUD DEPARTAMENTOS

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
 from .views import (
-                    Departamento_list,
-                    DepartamentoCreate,
-                    DepartamentoUpdate,
-                    DepartamentoDelete
+                    DocumentoCreate
                     )
 
 
 urlpatterns = [
-
-    path('list', Departamento_list.as_view(), name='list_departamentos'),
-    path('novo', DepartamentoCreate.as_view(), name='create_departamento'),
-    path('update/<int:pk>/', DepartamentoUpdate.as_view(), name='update_departamento'),
-    path('delete/<int:pk>/', DepartamentoDelete.as_view(), name='delete_departamento'),
-
+    path('novo/<int:funcionario_id>', DocumentoCreate.as_view(), name='create_documento'),
 ]
